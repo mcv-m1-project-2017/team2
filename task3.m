55eea74cc2a7eda17cd6ac2f9a7a5cf88cfccd2b
AnaCaballero89 <32468326+AnaCaballero89@users.noreply.github.com>
2017-10-15 13:00:49 +0200

    Add files via upload

--- task3.m
+++ task3.m
@@ -1,34 +1,54 @@
-function [masks_improved] = task3(masks, se, plots)
+% This function generates merged masks for red and blue and saves the
+% results in /block1/task3/masks
 
-masks_filled = cell(length(se),length(masks));
-masks_erode = cell(length(se),length(masks));
-masks_improved = cell(length(se),length(masks));
+function [all_image_data] = task3(all_data, bluemin, bluemax, redmin, redmax,data_path)
+    
+    tic
+    dbstop if error
 
-for n = 1:length(masks)
-    
-    figure;
-    
-    for ii=1:length(se)
-        
-        masks_filled{ii,n} = imfill(masks{1,n},'holes');
-        masks_erode{ii,n}=imerode(masks_filled{1,n},se{1,ii});
-        
-        %erode the mask with the different "brushes"
-        
-        masks_improved{ii,n}=imdilate(masks_erode{ii,n},se{1,ii});
-        
-        %dilate the eroded mask with the different "brushes"
-        
-        if strcmp(plots,'yes') == 1
-            
-            subplot(length(se),3,ii+ii*2-2);imshow(masks{1,n});title('mask');
-            subplot(length(se),3,ii+ii*2-1);imshow(masks_erode{ii,n});title(['eroded ' num2str(length(se{1,ii}))]);
-            subplot(length(se),3,ii+ii*2);imshow(masks_improved{ii,n});title(['eroded + dilated ' num2str(length(se{1,ii}))]);
-            %dilate the eroded mask with the same "brushes" used before
-        end
+    if nargin<5
+        data_path = pwd;
     end
+
+    if ~isdir('block1\task3\masks')
+        mkdir('block1\task3\masks');
+    end
+    
+    % Uncomment line 19 or line 22 to generate masks only for the
+    % validation dataset or only for the training dataset.
+    
+    % Only validation images
+    % all_data = all_data([all_data.validation]==1);
+    
+    % Only training images
+    % all_data = all_data([all_data.validation]==0);
+    
+    % list of all images
+    [~,idx,~] = unique({all_data.file_id});
+    % Area and form will be only of the first sign
+    all_image_data = all_data(idx);
+
+    % Loop on all the Images
+    %=======================
+    test_files_names = dir('test');
+    for ii = 3:size(test_files_names)
+        im_full_path = test_files_names(ii).name;
+
+        % Red mask
+        [BW_red,~] = createMaskForRed(imread(im_full_path), redmin, redmax);
+        
+        % Blue mask
+        [BW_blue,~] = createMaskForBlue(imread(im_full_path), bluemin, bluemax);
+        BW_blue = (~BW_blue);
+        
+        % Merge - the 2 maskes
+        BW = BW_red|BW_blue;
+        
+        writing_path = strcat('block1\task3\masks_test\',test_files_names(ii).name(1:end-4),'.png');
+        disp(writing_path);
+        imwrite(BW, writing_path);
+
+    end
+    toc
+
 end
-
-
-end
-
