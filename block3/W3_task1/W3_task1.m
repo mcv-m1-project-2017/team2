471e828f50ae0c9e0d7c339929e549efa7d9648d
noamor <noamor87@gmail.com>
2017-10-24 16:34:02 +0200

    T1

--- block3/W3_task1/W3_task1.m
+++ block3/W3_task1/W3_task1.m
@@ -45,33 +45,11 @@ end
 % masks_list{1} = test_mask;
 
 % according to statistics on training data-set
-if nargin < 4|| isempty(statistic_table)
-    minmax_area = [890,56000];
-    minmax_ratio = [0.43,1.42];
-    minmax_mc_col = [0.45, 0.58];
-    
-    minmax_ff(1,:)   =   [0.42,0.6]; % Triangale
-    minmax_ff(2,:)   =   [0.42,0.6]; % Triangale_Up
-    minmax_ff(3,:)   =   [0.49,0.89]; % Circle
-    minmax_ff(4,:)   =   [0.96,1.1]; % Rectangle
-    
-    minmax_mc_row(1,:)   =   [0.61,0.74]; % Triangale
-    minmax_mc_row(2,:)   =   [0.29,0.41]; % Triangale_Up
-    minmax_mc_row(3,:)   =   [0.44,0.55]; % Circle
-    minmax_mc_row(4,:)   =   [0.44,0.55]; % Rectangle
-else
-    %     Tri =
-    %     minmax_ff(1,:)  =   statistic_table(ismember(statistic_table.type,{'A','B'})).form_factor)
+if nargin < 4
+   statistic_table = [];
 end
-% if nargin < 4 || isempty(minmax_ratio)
-%     minmax_ratio = [0.43,1.42];
-% end
-% if nargin < 5 || isempty(minmax_ff)
-%     minmax_ff(1,:)   =   [0.48,0.54]; % Triangale
-%     minmax_ff(2,:)   =   [0.51,0.85]; % Circle
-%     minmax_ff(3,:)   =   [0.97,1.1]; % Rectangle
-% end
-if nargin<4
+
+if nargin<5
     plot_flag = false;
 end
 %=============================
@@ -90,7 +68,7 @@ for ii = 1: length(masks_list)
     % connect neigbour pixels
     %------------------------
     file_id = masks_list{ii}(1:9);
-    [ windowCandidates ] = find_bBox( cur_mask,file_id,[],plot_flag,fullfile(image_dir,[file_id,'.jpg']),ii );
+    [ windowCandidates ] = find_bBox( cur_mask,file_id,statistic_table,plot_flag,fullfile(image_dir,[file_id,'.jpg']),ii );
     % save mat file
     [ mask_out ] = mask_bbox( cur_mask,windowCandidates );
     out_mask_name = fullfile(out_dir,[file_id,'_mask.png']);
