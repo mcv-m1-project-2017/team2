f635efc3dcec342fdd5897248e1ce10dd108a63d
noamor <noamor87@gmail.com>
2017-10-25 18:27:44 +0200

    syntax bugs corrected

--- block3/W3_task4/W3_Task4.m
+++ block3/W3_task4/W3_Task4.m
@@ -3,10 +3,11 @@ function [precision, sensitivity, accuracy] = W3_Task4 (annotation_dir, detectio
 % annotation_dir = C:\Users\MI-CUENTA\team2\train\gt
 detection_file_list = dir(fullfile(detection_dir,'*.mat'));
 
+detection_file_list = {detection_file_list.name};
 
    
    TP=0; FN=0; FP=0;
-   for i=1:lenght(detection_file_list)
+   for ii=1:length(detection_file_list)
        
        file_id = detection_file_list{ii}(1:9);
        gt_filename = ['gt.', file_id, '.txt'];
