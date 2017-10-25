899f2e2031ca1959a83257457964ddf3297c37ff
AnaCaballero89 <32468326+AnaCaballero89@users.noreply.github.com>
2017-10-25 18:04:55 +0200

    Add files via upload

---
+++ block3/W3_task4/W3_Task4.m
@@ -0,0 +1,36 @@
+function [precision, sensitivity, accuracy] = W3_Task4 (annotation_dir, detection_dir)%(Ground_Truth_Bbox, Candidate_detection_BBox )
+
+% annotation_dir = C:\Users\MI-CUENTA\team2\train\gt
+detection_file_list = dir(fullfile(detection_dir,'*.mat'));
+
+
+   
+   TP=0; FN=0; FP=0;
+   for i=1:lenght(detection_file_list)
+       
+       file_id = detection_file_list{ii}(1:9);
+       gt_filename = ['gt.', file_id, '.txt'];
+       gt_filename = fullfile(annotation_dir,gt_filename);
+       
+       load(fullfile(detection_dir,detection_file_list{ii}));
+       
+       
+      [annotations, ~] = LoadAnnotations(gt_filename);
+    
+      [TP2,FN2,FP2] = PerformanceAccumulationWindow(windowCandidates, annotations);
+        % 'detections'        List of windows marking the candidate detections
+        % 'annotations'       List of windows with the ground truth positions of the objects
+
+         %correctDetection = BBoxDet ^ BBoxGT / BBoxDet U BBoxGT %>0.5
+    %      TN=0;%(size image x num images) - (tp+fn+fp)
+    %      correctDetection = ((TP +TN)/(FP+FN)) >0.5;
+    
+%         [precision, sensitivity, accuracy] = PerformanceEvaluationWindow(TP, FN, FP);
+
+        TP=TP+TP2;
+        FN=FN+FN2;
+        FP=FP+FP2;
+        
+   end
+   [precision, sensitivity, accuracy] = PerformanceEvaluationWindow(TP, FN, FP);
+end
