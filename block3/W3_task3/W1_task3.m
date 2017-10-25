68d5d244533900a431b5d48a4c5aa6a6ca2b8157
AnaCaballero89 <32468326+AnaCaballero89@users.noreply.github.com>
2017-10-25 18:04:01 +0200

    Update and rename W1_task4.m to W1_task3.m

--- block3/W3_task3/W1_task3.m
+++ block3/W3_task3/W1_task3.m
@@ -1,4 +1,4 @@
-function [ output_args ] = W1_task4( mask )
+function [ output_args ] = W1_task3( mask )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 Int_mask = integralImage(mask);
