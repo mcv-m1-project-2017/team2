27e1d2043d7a61befc4f87542a56037820446f80
noamor <noamor87@gmail.com>
2017-10-30 04:05:04 +0100

    .

--- block3/W3_task3/centerOfMass_Int.m
+++ block3/W3_task3/centerOfMass_Int.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function [ mass_center ] = centerOfMass_Int( int_mask )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
@@ -23,29 +22,3 @@ mass_center(1) = find(int_mask(:,end)>=midval,1,'first');
 mass_center(1) = mass_center(1)+ (midval-int_mask(mass_center(1),end))/midval;
 end
 
-=======
-function [ mass_center ] = centerOfMass_Int( int_mask )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-
-% doest change the meaning of the integral image
-mass_center = [0,0];
-int_mask = int_mask- int_mask(1,1);
-
-% the mass center will be exactly were there are 4 qurters with 1/4 the
-% pixels
-
-% for The Vertical mass center we can only look at the Buttom row
-
-midval = int_mask(end,end)/2;
-
-mass_center(2) = find(int_mask(end,:)>=midval,1,'first');
-mass_center(2) = mass_center(2)+ (midval-int_mask(end,mass_center(2)))/midval;
-% for The Horizontal mass center we can only look at the Buttom row
-
-
-mass_center(1) = find(int_mask(:,end)>=midval,1,'first');
-mass_center(1) = mass_center(1)+ (midval-int_mask(mass_center(1),end))/midval;
-end
-
->>>>>>> e11478986e49ffcb28f207ff6acb164669fea2b4
