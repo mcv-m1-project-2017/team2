86f96a31d952b9484cf22ef0106124373c887341
AnaCaballero89 <32468326+AnaCaballero89@users.noreply.github.com>
2017-11-02 20:05:42 +0100

    Add files via upload

--- block4/W4_task2/CalculateEdges.m
+++ block4/W4_task2/CalculateEdges.m
@@ -1,18 +1,34 @@
-function [] = CalculateEdges (I)
+function [] = CalculateEdges (img, bw)
     close all;
 
-    sobel = edge(I,'Sobel');
-    canny = edge(I,'Canny');
-    prewitt = edge(I,'Prewitt');
-    roberts = edge(I,'Roberts');
-    laplacianOfGausian = edge(I,'log');
-    zerocross = edge(I,'zerocross');
+    %GRAY
+    sobel_grey = edge(img,'Sobel');
+    canny_grey = edge(img,'Canny');
+    prewitt_grey = edge(img,'Prewitt');
+    roberts_grey = edge(img,'Roberts');
+    laplacianOfGausian_grey = edge(img,'log');
+    zerocross_grey = edge(img,'zerocross');
     
-    subplot(2,3,1), subimage(sobel); title('Sobel');
-    subplot(2,3,2), subimage(canny); title('Canny');
-    subplot(2,3,3), subimage(prewitt); title('Prewitt');
-    subplot(2,3,4), subimage(roberts); title('Roberts');
-    subplot(2,3,5), subimage(laplacianOfGausian); title('Laplacian of Gausian');
-    subplot(2,3,6), subimage(zerocross); title('zerocross');
+    % BW
+    sobel_bw = edge(bw,'Sobel');
+    canny_bw = edge(bw,'Canny');
+    prewitt_bw = edge(bw,'Prewitt');
+    roberts_bw = edge(bw,'Roberts');
+    laplacianOfGausian_bw = edge(bw,'log');
+    zerocross_bw = edge(bw,'zerocross');
     
+    subplot(4,3,1), imshow(sobel_grey); title('Grey Sobel');
+    subplot(4,3,2), imshow(canny_grey); title('Grey Canny');
+    subplot(4,3,3), imshow(prewitt_grey); title('Grey Prewitt');
+    subplot(4,3,4), imshow(roberts_grey); title('Grey Roberts');
+    subplot(4,3,5), imshow(laplacianOfGausian_grey); title('Grey Laplacian of Gausian');
+    subplot(4,3,6), imshow(zerocross_grey); title('Grey Zerocross');
+    
+    
+    subplot(4,3,7), imshow(sobel_bw); title('Binary Sobel');
+    subplot(4,3,8), imshow(canny_bw); title('Binary Canny');
+    subplot(4,3,9), imshow(prewitt_bw); title('Binary Prewitt');
+    subplot(4,3,10), imshow(roberts_bw); title('Binary Roberts');
+    subplot(4,3,11), imshow(laplacianOfGausian_bw); title('Binary Laplacian of Gausian');
+    subplot(4,3,12), imshow(zerocross_bw); title('Binary Zerocross');
 end 
--- block4/W4_task2/W4_task2.m
+++ block4/W4_task2/W4_task2.m
@@ -4,7 +4,7 @@ function [] = W4_task2(RGBimage)
    
    % Edges computed by canny, gradient magnitude or others.
    % on gray level image or contours from binary mask
-   CalculateEdges(g_img); %DONE
+   CalculateEdges(g_img,bw_img); %DONE
    
    %Model learning of edges (hand crafted, average or other)
    CalculateManualEdges(g_img);
@@ -14,5 +14,20 @@ function [] = W4_task2(RGBimage)
    
    % chamfer distance to classify
    ChamferDistance(g_img);
+   
+%   Template Matching (chamfer)  
+%    Given:
+%     • binary image, B, of edge and local feature locations
+%     • binary “edge template”, T, ofshape we want to match
+%    
+%    Let D be an array s.t. D(i,j) is the distance to the nearest “1” in B. This array is
+%    called the distance transform (DT) of B --> Matlab: bwdist()
+%    
+%    Goal: Find placement of T in D that minimizes the sum, M, of the DT
+%    multiplied by the pixel values in T
+%     – if T is an exact match to B at location (i,j) then M(i,j) = 0
+%     – if the edges in B are slightly displaced from their ideal locations in T, we still get
+%     a good match using the distance transform technique
+%    
        
 end
