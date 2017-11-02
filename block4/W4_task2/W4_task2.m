67290c75b9b71a4f5a0fcc980ff541baf252e85e
AnaCaballero89 <32468326+AnaCaballero89@users.noreply.github.com>
2017-11-02 19:46:34 +0100

    Add files via upload

--- block4/W4_task2/W4_task2.m
+++ block4/W4_task2/W4_task2.m
@@ -2,9 +2,17 @@ function [] = W4_task2(RGBimage)
    g_img = rgb2gray(RGBimage);
    bw_img = im2bw(g_img);
    
+   % Edges computed by canny, gradient magnitude or others.
+   % on gray level image or contours from binary mask
    CalculateEdges(g_img); %DONE
+   
+   %Model learning of edges (hand crafted, average or other)
    CalculateManualEdges(g_img);
+   
+   % Distance transform of test image
    DistanceTransform(g_img,bw_img); %DONE
+   
+   % chamfer distance to classify
    ChamferDistance(g_img);
        
 end
