a68cd38583c5357937d4c8102f43ac884f857ef7
AnaCaballero89 <32468326+AnaCaballero89@users.noreply.github.com>
2017-11-04 17:37:04 +0100

    Add files via upload

--- block4/W4_main.m
+++ block4/W4_main.m
@@ -19,4 +19,32 @@ W4_task1;
 W4_task2;
 
 % Task 3
-W4_task3;
+%%
+method =1;
+if (method==1)
+    %METHOD1: Global approach
+    annotation_dir = Given_gt_path;
+    out_dir_int =;
+    provided_mask_path = '../train/mask';
+
+elseif (method==2)
+    %METHOD2: Using CC
+    annotation_dir = ;%CCL_results_path;
+    out_dir_int =fullfile(pwd,'Results/CCL');
+    provided_mask_path = '../train/mask';
+
+else
+    %METHOD3: Using SLW
+    annotation_dir = ;%SLW_results_path;
+    out_dir_int = fullfile(pwd,'Results/Sliding_window');
+    provided_mask_path = '../train/mask';
+
+end
+%%
+[region_out_int, pix_out_int] = W4_Task3 (annotation_dir, out_dir_int,provided_mask_path,out_dir_int);
+Evaluation_region = struct2table(region_out_int);
+display(Evaluation_region);
+pix_out_int.time_per_frame = time_per_frame;
+
+Evaluation_pixel = struct2table(pix_out_int);
+display(Evaluation_pixel);
