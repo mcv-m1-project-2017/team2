3a5f241023d4ce740c76684b707f4dc463931b46
Lorenzo <smemo23@gmail.com>
2017-11-11 16:16:32 +0100

    To be finished

--- block5/W5_task2/W5_task2.m
+++ block5/W5_task2/W5_task2.m
@@ -1,4 +1,37 @@
+% Call from cmd with:
+% a = imread('../train/00.000948.jpg');
+% out = CircularHough_Grd(double(edge(im2bw(a),'canny')), [15, 80]);
 function [] = W5_task2(img, out_dir)
-imshow(img);
+BW = edge(img, 'canny');
 
+[H,T,R] = hough(BW);
+imshow(H,[],'XData',T,'YData',R,...
+            'InitialMagnification','fit');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
+
+
+P  = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
+x = T(P(:,2)); y = R(P(:,1));
+plot(x,y,'s','color','white');
+
+
+lines = houghlines(BW,T,R,P,'FillGap',5,'MinLength',7);
+figure, imshow(img), hold on
+max_len = 0;
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+   % Plot beginnings and ends of lines
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+   % Determine the endpoints of the longest line segment
+   len = norm(lines(k).point1 - lines(k).point2);
+   if ( len > max_len)
+      max_len = len;
+      xy_long = xy;
+   end
+end
 end
